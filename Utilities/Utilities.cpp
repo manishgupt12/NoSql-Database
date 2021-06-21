406f33016c3971dde5b5546cbf9df67c33a45e96
Manish Gupta <85456258+manishgupt12@users.noreply.github.com>
2021-06-21 06:47:46 +0530

    Add files via upload

---
+++ Utilities/Utilities.cpp
@@ -0,0 +1,17 @@
+// Utilities.cpp
+
+#include "Utilities.h"
+
+#ifdef UTILITITES
+
+
+
+int main()
+{
+  Title("this is a lead title");
+  putLine();
+  subTitle("this is a sub-title");
+  putLines(4);
+}
+
+#endif // UTILITITES
